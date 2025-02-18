c5f208941117019c0cf78b023d5bab93717f5b01
Ugnius Spaicys <ugnius.bootcamp@gmail.com>
2025-02-18 10:28:20 +0200

    animal factory tests

---
+++ SavannaApp.Tests/Factory/AnimalFactoryTests/CreateAnimal.cs
@@ -0,0 +1,61 @@
+﻿using SavannaApp.Data.Entities.Animals;
+using SavannaApp.Data.Entities;
+using SavannaApp.Data.Interfaces.Game;
+using SavannaApp.Data.Interfaces;
+using SavannaApp.Data.Factory;
+using SavannaApp.Data.Util;
+using SavannaApp.Data.Entities.MovementStrategies;
+
+namespace SavannaApp.Tests.Factory.AnimalFactoryTests
+{
+    [TestClass]
+    public class CreateAnimal
+    {
+        private IAnimalFactory _factory = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var mapManager = new MapManager();
+            var hunterMovement = new HunterMovement(mapManager);
+            var prayMovement = new PrayMovement(mapManager);
+
+            _factory = new AnimalFactory(hunterMovement, prayMovement);
+        }
+        [TestMethod]
+        public void CreateAnimal_ShouldReturnLion_WhenAnimalTypeIsLion()
+        {
+            // Arrange
+            var animalType = typeof(Lion);
+            var x = 5;
+            var y = 10;
+
+            // Act
+            var animal = _factory.CreateAnimal(animalType, x, y);
+
+            // Assert
+            Assert.IsNotNull(animal);
+            Assert.IsInstanceOfType(animal, typeof(Lion));
+            Assert.AreEqual(x, animal.Position.X);
+            Assert.AreEqual(y, animal.Position.Y);
+        }
+
+        [TestMethod]
+        public void CreateAnimal_ShouldReturnAntelope_WhenAnimalTypeIsAntelope()
+        {
+            // Arrange
+            var animalType = typeof(Antelope);
+            var x = 15;
+            var y = 20;
+
+            // Act
+            var animal = _factory.CreateAnimal(animalType, x, y);
+
+            // Assert
+            Assert.IsNotNull(animal);
+            Assert.IsInstanceOfType(animal, typeof(Antelope));
+            Assert.AreEqual(x, animal.Position.X);
+            Assert.AreEqual(y, animal.Position.Y);
+        }
+    }
+}
