42ed1fb9d289e2e0bee560fd1bc7df583fcd6267
Ugnius Spaicys <ugnius.bootcamp@gmail.com>
2025-02-18 10:22:13 +0200

    pray, random movement tests

---
+++ SavannaApp.Tests/Entities/MovementStrategiesTests/Pray/Move.cs
@@ -0,0 +1,89 @@
+﻿using SavannaApp.Data.Entities.Animals;
+using SavannaApp.Data.Entities.MovementStrategies;
+using SavannaApp.Data.Entities;
+using SavannaApp.Data.Interfaces.Game;
+using SavannaApp.Data.Interfaces.Map;
+using SavannaApp.Data.Interfaces;
+using SavannaApp.Data.Util;
+
+namespace SavannaApp.Tests.Entities.MovementStrategiesTests.Pray
+{
+    [TestClass]
+    public class Move
+    {
+        private Animal lion = null!;
+        private Animal antelope = null!;
+        private IMovement hunter = null!;
+        private IMovement pray = null!;
+        private IMapManager mapManager = null!;
+        private IMap map = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            map = new Map(20, 20);
+            mapManager = new MapManager();
+            hunter = new HunterMovement(mapManager);
+            pray = new PrayMovement(mapManager);
+            lion = new Lion(1, 9, 9, "L", 5, 5, 5, hunter);
+            antelope = new Antelope(2, 10, 12, "A", 5, 5, 5, pray);
+        }
+
+        [TestMethod]
+        public void Move_NoLionsAround_ShouldReturnFalse() 
+        {
+            //Arrange
+            map.SetAnimal(antelope);
+            var expected = false;
+
+            //Act
+            var result = pray.Move(antelope, map);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Move_AntelopeShouldRunFromLion_ShouldReturnTrue() 
+        {
+            //Arrange
+            map.SetAnimal(antelope);
+            map.SetAnimal(lion);
+            var expected = true;
+            var initialDistance = antelope.DistanceTo(lion.Position.X, lion.Position.Y);
+
+            //Act
+            var result = pray.Move(antelope, map);
+            var distanceAfterMove = antelope.DistanceTo(lion.Position.X, lion.Position.Y);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+            Assert.IsTrue(distanceAfterMove > initialDistance);
+        }
+
+        [TestMethod]
+        public void Move_NoLionsInVision_ShouldReturnFalse()
+        {
+            //Arrange
+            lion = new Lion(1, 9, 9, "L", 5, 5, 5, hunter);
+            antelope = new Antelope(2, 10, 12, "A", 5, 2, 5, pray);
+
+            map.SetAnimal(antelope);
+            map.SetAnimal(lion);
+            var expected = false;
+
+            //Act
+            var result = pray.Move(antelope, map);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            map = new Map(20, 20);
+        }
+
+    }
+}
---
+++ SavannaApp.Tests/Entities/MovementStrategiesTests/Random/Move.cs
@@ -0,0 +1,47 @@
+﻿using SavannaApp.Data.Entities.Animals;
+using SavannaApp.Data.Entities.MovementStrategies;
+using SavannaApp.Data.Entities;
+using SavannaApp.Data.Interfaces.Game;
+using SavannaApp.Data.Interfaces.Map;
+using SavannaApp.Data.Interfaces;
+using SavannaApp.Data.Util;
+
+namespace SavannaApp.Tests.Entities.MovementStrategiesTests.Random
+{
+    [TestClass]
+    public class Move
+    {
+        private Animal lion = null!;
+        private IMovement random = null!;
+        private IMap map = null!;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            random = new RandomMovement();
+            map = new Map(4, 4);
+            lion = new Lion(1, 1, 1, "L", 1, 1, 5, random);
+        }
+
+        [TestMethod]
+        public void Move_ShouldNotExceedBuffer() 
+        {
+            //Arrange
+            var xStart = 0;
+            var xEnd = map.Width;
+            var yStart = 0;
+            var yEnd = map.Height;
+            map.SetAnimal(lion);
+
+            //Act and Assert
+            for (int i = 0; i < 100; i++) 
+            {
+                random.Move(lion, map);
+                Assert.IsTrue(xStart != lion.Position.X);
+                Assert.IsTrue(xEnd != lion.Position.X);
+                Assert.IsTrue(yStart != lion.Position.Y);
+                Assert.IsTrue(yEnd != lion.Position.Y);
+            }
+        }
+    }
+}
