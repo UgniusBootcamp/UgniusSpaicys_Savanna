781c2f0c67e977ecf1b0b6450dc709b5ef11017b
Ugnius Spaicys <ugnius.bootcamp@gmail.com>
2025-02-28 11:18:50 +0200

    not found page

--- Frontend/SavannaWeb/src/App.jsx
+++ Frontend/SavannaWeb/src/App.jsx
@@ -1,30 +1,33 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
-import routes from './constants/routes'
-import Home from './pages/Home'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import Savanna from './pages/game/Savanna'
-import Error from './pages/error/Error'
-import NoAccess from './pages/error/NoAccess'
-import Layout from './pages/Layout'
+import { BrowserRouter, Route, Routes } from 'react-router';
+import routes from './constants/routes';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Error from './pages/error/Error';
+import NoAccess from './pages/error/NoAccess';
+import NotFound from './pages/error/NotFound';
+import Savanna from './pages/game/Savanna';
+import Home from './pages/Home';
+import Layout from './pages/Layout';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path={routes.home} element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path={routes.login} element={<Login/>}/>
-          <Route path={routes.register} element={<Register/>}/>
-          <Route path={routes.savanna} element={<Savanna/>}/>
-          <Route path={routes.error} element={<Error/>}/>
-          <Route path={routes.noAccess} element={<NoAccess/>}/>
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path={routes.home} element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path={routes.login} element={<Login />} />
+            <Route path={routes.register} element={<Register />} />
+            <Route path={routes.savanna} element={<Savanna />} />
+            <Route path={routes.error} element={<Error />} />
+            <Route path={routes.noAccess} element={<NoAccess />} />
+            <Route path={routes.NotFound} element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
